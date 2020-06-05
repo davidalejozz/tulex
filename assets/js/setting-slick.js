f2315787783c9303a09258ec350a7542b1b639f3
dannyunico <danielbermudezreyes@gmail.com>
2020-06-05 15:39:06 -0400

    home

---
+++ assets/js/setting-slick.js
@@ -0,0 +1,46 @@
+
+$('.slider-main__paquetes').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$('.slider-sobre__nosotros').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows:false
+});
+
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});
